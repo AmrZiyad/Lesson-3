1b83be430126373dfffd129a7f04405f67aa3904
zxyd_amr <mail.amrziyad@gmail.com>
2026-05-16 13:56:03 +0300

    Add files via upload

---
+++ Code Lesson 3 Task 2.py
@@ -0,0 +1,22 @@
+name = "Penguine"
+age = 15
+is_Student = True
+weight = 38.5
+print("Name:", name)
+print("Data type of name ", type (name))
+
+print("Age: ", age)
+print("Data type of age is", type (age))
+
+print("Is Student: ", is_Student)
+print("Data type of is_Student is", type (is_Student))
+
+print("Weight: ", weight)
+print("Data type of weight is", type (weight))
+
+print("/n Data type after typecasting")
+age =str(age)
+print(age)
+weight = int(weight)
+print(weight)
+
